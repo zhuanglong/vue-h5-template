42271381c8ad70d32c9ec651ee13e40ae13e4959
zhuanglong <2766428466@qq.com>
2020-07-09 23:31:33 +0800

    eslint

---
+++ .prettierrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'printWidth': 120,
+  'tabWidth': 2,
+  'singleQuote': true,
+  'trailingComma': 'none',
+  'semi': true,
+  'wrap_line_length': 120,
+  'wrap_attributes': 'auto',
+  'proseWrap': 'always',
+  'arrowParens': 'avoid',
+  'bracketSpacing': true,
+  'jsxBracketSameLine': true,
+  'useTabs': false,
+  'eslintIntegration':true,
+  'overrides': [
+    {
+      'files': '.prettierrc',
+      'options': {
+        'parser': 'json'
+      }
+    }
+  ],
+  'endOfLine': 'auto'
+}
