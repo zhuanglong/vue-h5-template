f095e741642db162cc0bd3cdec40ed5cd024ce7a
zhuanglong <2766428466@qq.com>
2020-07-13 21:50:12 +0800

    全局环境变量

---
+++ src/config/env.development.js
@@ -0,0 +1,9 @@
+// 正式
+module.exports = {
+  title: 'vue-h5-template',
+  baseUrl: 'http://localhost:9018', // 项目地址
+  baseApi: 'https://test.xxx.com/api', // 本地 api 请求地址，如果你使用了代理，请设置成'/'
+  APPID: 'xxx',
+  APPSECRET: 'xxx',
+  $cdn: 'https://imgs.xxx.com'
+};
---
+++ src/config/env.production.js
@@ -0,0 +1,9 @@
+// 正式
+module.exports = {
+  title: 'vue-h5-template',
+  baseUrl: 'https://prod.xxx.com', // 项目地址
+  baseApi: 'https://prod.xxx.com/api', // api 请求地址
+  APPID: 'xxx',
+  APPSECRET: 'xxx',
+  $cdn: 'https://imgs.xxx.com'
+};
