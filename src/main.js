6ee181eb69974eaf54a9b1f75400b3cd2602038f
zhuanglong <2766428466@qq.com>
2020-07-13 22:01:29 +0800

    兼容 IE

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
+// 兼容 IE
+// https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md#babelpolyfill
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
-console.log(process.env.VUE_APP_ENV);
+
 new Vue({
   router,
   store,
